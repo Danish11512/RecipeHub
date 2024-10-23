71cbc851449cae9cb9b6f05cdf72311bb2119c58
Danish11512 <danish.faruqi1@gmail.com>
2024-10-23 14:16:11 -0400

    login logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -46,25 +46,47 @@ const Login = () => {
         <div className="container">
             <div className="columns is-centered is-vcentered">
                 <div className="column is-half">
+                    {errorMessage &&
+                        <section class="hero is-danger">
+                            <div class="hero-body">
+                                <p class="title">Error</p>
+                                <p class="subtitle">{errorMessage}</p>
+                            </div>
+                        </section>
+                    }
                     <div className="card">
                         <div className="card-content">
                             <h1 className="title has-text-centered">Login</h1>
-                            <form>
+                            <form onSubmit={onSubmit}>
                                 <div className="field">
                                     <label className="label">Email</label>
                                     <div className="control">
-                                        <input className="input" type="email" placeholder="Enter your email" />
+                                        <input className="input"
+                                            type="email"
+                                            placeholder="Enter your email"
+                                            autoComplete='email'
+                                            required
+                                            value={email}
+                                            onChange={(e) => { setEmail(e.target.value) }} />
                                     </div>
                                 </div>
                                 <div className="field">
                                     <label className="label">Password</label>
                                     <div className="control">
-                                        <input className="input" type="password" placeholder="Enter your password" />
+                                        <input className="input"
+                                            type="password"
+                                            placeholder="Enter your password"
+                                            autoComplete='current-password'
+                                            required
+                                            value={password}
+                                            onChange={(e) => { setPassword(e.target.value) }} />
                                     </div>
                                 </div>
                                 <div className="field">
                                     <div className="control has-text-centered">
-                                        <button className="button is-primary">Login</button>
+                                        <button className="button is-primary" type="submit" disabled={isSigningIn} >
+                                            {isSigningIn ? 'Signing In...' : 'Sign In'}
+                                        </button>
                                     </div>
                                 </div>
                             </form>
@@ -79,6 +101,11 @@ const Login = () => {
                                 <span>
                                     <a onClick={goToSignup}>Sign Up</a>
                                 </span>
+                            </p>
+                            <p className="card-footer-item">
+                                <button disabled={isSigningIn} onClick={(e) => { onGoogleSignIn(e) }}>
+                                    <a>{isSigningIn ? 'Signing In...' : 'Continue with Google'} </a>
+                                </button>
                             </p>
                         </footer>
                     </div>
